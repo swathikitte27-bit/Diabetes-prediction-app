ac6c09955fb9e4eae98438a58eca2546224a6273
swathikitte27-bit <swathikitte27@gmail.com>
2026-05-23 12:06:13 +0530

    Add files via upload

---
+++ predictive system.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle
+
+loaded_model = pickle.load(open('C:/Users/swath/OneDrive/Desktop/deploy diabetes Prediction/trained_model.sav', 'rb'))
+scaler = pickle.load(open('C:/Users/swath/OneDrive/Desktop/deploy diabetes Prediction/scaler.sav', 'rb'))
+
+input_data = (3,78,50,32,88,31,0.248,26) 
+
+input_data_as_numpy_array = np.asarray(input_data)
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+
+std_data = scaler.transform(input_data_reshaped) 
+
+prediction = loaded_model.predict(std_data) 
+print(prediction)
+
+if prediction[0] == 0:
+  print('The person is not diabetic')
+else:
+  print('The person is diabetic')
